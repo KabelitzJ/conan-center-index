776bef93b0e9b585b99712e1220c672a17f5248d
jxk10011 <jonas.kabelitz@belden.com>
2025-05-05 11:32:56 +0200

    added implot/0.16-docking

--- recipes/implot/all/conanfile.py
+++ recipes/implot/all/conanfile.py
@@ -38,7 +38,9 @@ class ImplotConan(ConanFile):
             self.options.rm_safe("fPIC")
 
     def requirements(self):
-        if Version(self.version) >= "0.14":
+        if Version(self.version) == "0.16-docking":
+            self.requires("imgui/1.91.5-docking", transitive_headers=True)
+        elif Version(self.version) >= "0.14":
             self.requires("imgui/1.90.5", transitive_headers=True)
         elif Version(self.version) >= "0.13":
             # imgui 1.89 renamed ImGuiKeyModFlags_* to  ImGuiModFlags_*
