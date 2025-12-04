7008b8ab33d26e679a6fb9860b93876dfced57f0
KabelitzJ <jonas-kabelitz@gmx.de>
2025-12-04 12:14:26 +0100

    "fixed" easy_profiler

--- recipes/easy_profiler/all/conanfile.py
+++ recipes/easy_profiler/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, save
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, save, replace_in_file
 from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
 from conan.tools.scm import Version
 import os
@@ -33,7 +33,7 @@ class EasyProfilerConan(ConanFile):
     @property
     def _min_cppstd(self):
         return 11
-    
+
     @property
     def _compilers_minimum_version(self):
         return {
@@ -41,21 +41,21 @@ class EasyProfilerConan(ConanFile):
             "clang": "3.3",
             "apple-clang": "8.0",
         }
-    
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def export_sources(self):
         export_conandata_patches(self)
-    
+
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
-    
+
     def validate(self):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
@@ -74,6 +74,7 @@ class EasyProfilerConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"), "cmake_minimum_required(VERSION 3.0)", "cmake_minimum_required(VERSION 3.5...${CMAKE_VERSION})")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -83,7 +84,7 @@ class EasyProfilerConan(ConanFile):
 
         deps = CMakeDeps(self)
         deps.generate()
-        
+
     def build(self):
         apply_conandata_patches(self)
         cmake = CMake(self)
